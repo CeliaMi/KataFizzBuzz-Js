2e19a9359143a4437d302f3d03b786e86f7ace62
Celia Millán <celiamillanalemany@gmail.com>
2023-08-18 19:42:48 +0200

    third test passed

--- TDD-FizzBuzz/src/fizzBuzz.js
+++ TDD-FizzBuzz/src/fizzBuzz.js
@@ -1,9 +1,10 @@
 function fizzBuzz(number){
     
-    if(number % 3 === 0) {
+    if(number % 15 === 0) {
+        return('FizzBuzz')
+    } else if (number % 3 === 0) {
         return('Fizz')
-    }
-    else if (number % 5 === 0) {
+    } else if (number % 5 === 0) {
         return('Buzz')
     }
 
