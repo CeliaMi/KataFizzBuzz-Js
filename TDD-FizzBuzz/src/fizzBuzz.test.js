0e1f117a6784c5c3e28e64f1e41a37286dc0f089
Celia Millán <celiamillanalemany@gmail.com>
2023-08-18 19:27:00 +0200

    second test

--- TDD-FizzBuzz/src/fizzBuzz.test.js
+++ TDD-FizzBuzz/src/fizzBuzz.test.js
@@ -1,6 +1,13 @@
 const fizzBuzz = require("../src/fizzBuzz.js")
+
 //primera premisa
 test("Si el número es 3, debe imprimir Fizz", () => {
     const result = fizzBuzz(3);
     expect(result).toBe('Fizz');
+})
+
+//segunda premisa
+test("Si el número es 5, debe imprimir Buzz", () => {
+    const result = fizzBuzz(5);
+    expect(result).toBe('Buzz');
 })
